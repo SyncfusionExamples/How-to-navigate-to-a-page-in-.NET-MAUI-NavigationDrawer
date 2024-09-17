d2530c7a9fc214a560e5fbbc740cb77b9299672a
Swetha <swetha.sf3977@gmail.com>
2024-09-17 17:05:19 +0530

    Updated sample

---
+++ NavigationDrawer/NavigationDrawer/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui.Platform;
+using Windows.Networking.NetworkOperators;
+
+namespace NavigationDrawer {
+    public partial class MainPage : ContentPage {
+        
+
+        public MainPage() {
+            InitializeComponent();
+        }
+        private async void Bt_Clicked(object sender, EventArgs e) {
+            await Navigation.PushAsync(new NewPage());
+        }
+        private void hamburgerButton_Clicked(object sender, EventArgs e) {
+            navigationDrawer.ToggleDrawer();
+        }
+
+    }
+
+}
---
+++ NavigationDrawer/NavigationDrawer/NewPage.cs
@@ -0,0 +1,15 @@
+namespace NavigationDrawer;
+
+public class NewPage : ContentPage
+{
+	public NewPage()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
